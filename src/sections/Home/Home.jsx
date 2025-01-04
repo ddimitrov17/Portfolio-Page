663b1aefeb02010b91f8e59b03f58e43c3441a7c
Dimitar Dimitrov <dimitardimitrov0317@gmail.com>
2025-01-04 18:20:32 +0200

    toggleDivsVisibility function on laptop click

--- src/sections/Home/Home.jsx
+++ src/sections/Home/Home.jsx
@@ -1,23 +1,32 @@
 import { Canvas } from '@react-three/fiber';
-import { Suspense } from 'react';
+import { Suspense, useState } from 'react';
 import { PerspectiveCamera, OrbitControls } from "@react-three/drei";
 import VirtualDesk from '../../components/VirtualDesk';
 import CanvasLoader from '../../components/CanvasLoader';
 
 export default function Home() {
+    const [showDivs, setShowDivs] = useState(true);
+
+    const toggleDivsVisibility = () => {
+        setShowDivs(prev => !prev);
+    };
+
     return (
         <section id="home" className="relative w-[1200px] h-[600px] mx-auto p-16">
-            <div className='absolute left-[100px] top-[95px] flex flex-col gap-10'>
-                <div className="bg-SocialNetworkBackground w-[120px] h-[110px] bg-cover z-50 rounded-xl border-4 border-aboutBorder transition-transform duration-300 hover:scale-110 cursor-pointer"></div>
-                <div className="bg-TripAppBackground w-[120px] h-[120px] bg-cover z-50 rounded-xl border-4 border-aboutBorder transition-transform duration-300 hover:scale-110 cursor-pointer"></div>
-                <div className="bg-BookAppBackground w-[120px] h-[100px] bg-cover z-50 rounded-xl border-4 border-aboutBorder transition-transform duration-300 hover:scale-110 cursor-pointer"></div>
-            </div>
+            {showDivs && (
+                <div className='absolute left-[100px] top-[95px] flex flex-col gap-10'>
+                    <div className="bg-SocialNetworkBackground w-[120px] h-[110px] bg-cover z-50 rounded-xl border-4 border-aboutBorder transition-transform duration-300 hover:scale-110 cursor-pointer"></div>
+                    <div className="bg-TripAppBackground w-[120px] h-[120px] bg-cover z-50 rounded-xl border-4 border-aboutBorder transition-transform duration-300 hover:scale-110 cursor-pointer"></div>
+                    <div className="bg-BookAppBackground w-[120px] h-[100px] bg-cover z-50 rounded-xl border-4 border-aboutBorder transition-transform duration-300 hover:scale-110 cursor-pointer"></div>
+                </div>
+            )}
             <Canvas className="absolute top-0 left-0 w-full h-full z-10 border border-aboutBorder bg-[#262627] rounded-xl">
                 <Suspense fallback={<CanvasLoader />}>
                     <VirtualDesk
                         scale={0.048}
                         position={[1, 0, 0]}
                         rotation={[0, 2.3, 0]}
+                        onLaptopClick={toggleDivsVisibility} 
                     />
                 </Suspense>
                 <PerspectiveCamera makeDefault position={[-1, 9, 13]} />
