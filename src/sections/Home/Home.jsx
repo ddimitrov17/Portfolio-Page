4d6a89ae64833e275d2fda68c5b9e9d1795956dd
Dimitar Dimitrov <dimitardimitrov0317@gmail.com>
2025-01-04 18:30:50 +0200

    tip div added on top of the home canvas

--- src/sections/Home/Home.jsx
+++ src/sections/Home/Home.jsx
@@ -13,6 +13,11 @@ export default function Home() {
 
     return (
         <section id="home" className="relative w-[1200px] h-[600px] mx-auto p-16">
+            {/* TIP Button above the Canvas */}
+            <div className='absolute bottom-20 left-1/2 transform -translate-x-1/2 z-50 w-[450px] p-1 h-[40px] border border-aboutBorder bg-aboutSectionColor rounded-lg text-white font-generalsans text-sm font-semibold flex justify-center items-center'>
+                TIP: Click on the Laptop in order to hide/unhide the project logos.
+            </div>
+
             {showDivs && (
                 <div className='absolute left-[100px] top-[95px] flex flex-col gap-10'>
                     <div className="bg-SocialNetworkBackground w-[120px] h-[110px] bg-cover z-50 rounded-xl border-4 border-aboutBorder transition-transform duration-300 hover:scale-110 cursor-pointer"></div>
@@ -20,13 +25,14 @@ export default function Home() {
                     <div className="bg-BookAppBackground w-[120px] h-[100px] bg-cover z-50 rounded-xl border-4 border-aboutBorder transition-transform duration-300 hover:scale-110 cursor-pointer"></div>
                 </div>
             )}
+
             <Canvas className="absolute top-0 left-0 w-full h-full z-10 border border-aboutBorder bg-[#262627] rounded-xl">
                 <Suspense fallback={<CanvasLoader />}>
                     <VirtualDesk
                         scale={0.048}
                         position={[1, 0, 0]}
                         rotation={[0, 2.3, 0]}
-                        onLaptopClick={toggleDivsVisibility} 
+                        onLaptopClick={toggleDivsVisibility}
                     />
                 </Suspense>
                 <PerspectiveCamera makeDefault position={[-1, 9, 13]} />
